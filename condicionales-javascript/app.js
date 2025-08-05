72db6ddd495813abba754f62b7ca4f68af125a54
Juan-cripton16 <polomercadojuan06@gmail.com>
2025-08-05 13:27:27 -0500

    condicionales terminado

--- condicionales-javascript/app.js
+++ condicionales-javascript/app.js
@@ -1,12 +1,33 @@
-let nombre = prompt("Ingresa tu nombre:");
+//let nombre = prompt("Ingresa tu nombre:");
+//let edad = prompt("Ingresa tu edad:");
+
+//if (edad >= 18) {
+// alert("Puedes ingresar a la discoteca.");
+//} else {
+// alert("No puedes ingresar a la discoteca.");
+//}
+
+//if (nombre.toLowerCase() === "mario" || nombre.toLowerCase() === "carlos") {
+// // alert("eres cliente VIP.");
+//}
+// EJRCICIO 2
+
+let estatura = prompt("Ingresa tu estatura:");
+let velocidad = prompt("Ingresa tu vcelocidad:");
 let edad = prompt("Ingresa tu edad:");
 
-if (edad >= 18) {
-  alert("Puedes ingresar a la discoteca.");
-} else {
-  alert("No puedes ingresar a la discoteca.");
-}
+if( estatura >=170 , velocidad >=27 , edad >=18 )   {
+  alert("felicidades cumples con los requisitos para ingresar al equipo");
+} 
+if (edad >=18 ) {
+  alert("felicidades , vas para las ligas mayores");
+} 
 
-if (nombre.toLowerCase() === "mario" || nombre.toLowerCase() === "carlos") {
-  alert("eres cliente VIP.");
-}
+if (edad <=18 ) {
+  alert("felicidades , vas para las ligas menorees");
+} 
+
+
+
+
+
